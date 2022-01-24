96bb4015884bafc38bb8bad44dae03ec0f38db81
kimminkyu <kmk50009@naver.com>
2022-01-25 01:09:14 +0900

    listening, lifting state up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Expenses from "./components//Expenses/Expenses";
+import React from "react";
+import NewExpense from "./components/Expenses/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -23,9 +25,14 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js')
+    console.log(expense)
+  }
+
   return (
     <div>
-      <h2>Let's get started!!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
